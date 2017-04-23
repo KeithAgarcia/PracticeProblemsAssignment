0d57d680197999b12c020aaafe834c8876411681
Keith Garcia <Keith@Keiths-MacBook.local>
2017-04-22 23:57:37 -0400

    update

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -3,6 +3,37 @@ package com.theironyard.charlotte;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+      //  FizzBuzz(7);
+        fibseq(10);
+
+
     }
+
+    public static String FizzBuzz(int x) {
+        if ((x < 100) && (x > 0)) {
+            if (x % 5 == x % 3) {
+                return "FizzBuzz";
+            }
+            if (x % 3 == 0) {
+                return "Fizz";
+
+            }
+            if (x % 5 == 0) {
+                return "Buzz";
+            }
+        }
+        return String.valueOf(x);
+    }
+
+            public static int [] fibseq(int x) {
+                int[] fibs = new int[x];
+                fibs[0] = 1;
+                fibs[1] = 1;
+                for(int i = 2; i < x; i++){
+                    fibs[i] =fibs[i - 1] + fibs[i -2];
+                }
+                return fibs;
+            }
 }
+
+
--- test/com/theironyard/charlotte/MainTest.java
+++ test/com/theironyard/charlotte/MainTest.java
@@ -1,8 +1,12 @@
 package com.theironyard.charlotte;
 
+import org.junit.Test;
+
 /**
  * Created by Ben on 12/13/16.
  */
 public class MainTest {
+
+
 
 }
